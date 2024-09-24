b424abe59d780e08c0c949578b5adba9b4c0fefe
LioTang <97962928+LioTang@users.noreply.github.com>
2024-09-23 22:14:18 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,160 @@
+while True:
+    import random
+    
+    def get_random_letter(letter_bank):
+        return random.choice(letter_bank)
+    
+    letter_bank = "GAVLISTCMDENQKRFYPHW"
+    random_letter = get_random_letter(letter_bank)
+    
+    if random_letter=="G":
+        print("Random Letter1:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "glycine":
+            print("nice")
+        else:
+            print("wrong")
+                
+    elif random_letter=="A":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "alanine":
+            print("nice2")
+        else:
+            print("wrong")
+            
+    elif random_letter=="L":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "leucine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="I":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "isoleucine":
+            print("nice2")
+        else:
+            print("wrong")
+            
+    elif random_letter=="S":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "serine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="T":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "threonine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="C":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "cystine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="M":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "methonine":
+            print("nice2")
+        else:
+            print("wrong")
+            
+    elif random_letter=="D":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "aspartate":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="E":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "glutamate":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="N":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "asparagine":
+            print("nice2")
+        else:
+            print("wrong")
+            
+    elif random_letter=="Q":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "glutamine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="K":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "lysine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="R":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "arginine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="F":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "phenylalanine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="Y":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "tyrosine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="P":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "proline":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="H":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "histidine":
+            print("nice2")
+        else:
+            print("wrong")
+
+    elif random_letter=="W":
+        print("Random Letter2:", random_letter)
+        answer = input("enter answer: ")
+        if answer == "tryptophan":
+            print("nice2")
+        else:
+            print("wrong")
